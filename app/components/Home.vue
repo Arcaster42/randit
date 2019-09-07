65719b192a4eec7102f0aa5f7405aef6703d5c2e
arcaster42 <tj.epperson070@gmail.com>
2019-09-07 20:14:31 +0900

    implemented home as main view

---
+++ app/components/Home.vue
@@ -0,0 +1,59 @@
+<template>
+    <Page class="page">
+        <ActionBar class="action-bar">
+            <Label class="action-bar-title" :text="getView"></Label>
+        </ActionBar>
+        <StackLayout>
+            <Login v-if="getView==='Login'"/>
+            <Register v-if="getView==='Register'"/>
+        </StackLayout>
+    </Page>
+</template>
+
+<script>
+import Login from './Login'
+import Register from './Register'
+    export default {
+        components: {
+            Login,
+            Register
+        },
+        computed: {
+            getView() {
+                return this.$store.state.view;
+            }
+        },
+        methods: {
+            loginTap() {
+                this.$store.commit('setView', 'Board')
+            }
+        }
+    };
+</script>
+
+<style scoped lang="scss">
+    // Start custom common variables
+    @import '../app-variables';
+    // End custom common variables
+
+    // Custom styles
+    .fa {
+        color: $accent-dark;
+    }
+
+    .info {
+        font-size: 20;
+    }
+
+    .input {
+        border-color: seagreen;
+        border-width: 5px;
+        border-radius: 30%;
+        padding: 20px;
+        width: 80%;
+    }
+
+    .layout {
+        background-color: whitesmoke;
+    }
+</style>
