e6e1fab19e2098073ba07853da5f87f866157807
arcaster42 <tj.epperson070@gmail.com>
2019-09-07 20:14:58 +0900

    skeleton finished

---
+++ app/components/Login.vue
@@ -0,0 +1,53 @@
+<template>
+    <FlexboxLayout class="layout" flexDirection="column" justifyContent="center">
+        <Label alignSelf="center" text="Username"/>
+        <TextField class="input" alignSelf="center" v-model="username" hint="Enter Username"/>
+        <Label alignSelf="center" text="Password"/>
+        <TextField class="input" alignSelf="center" v-model="password" hint="Enter Username"/>
+        <Button class="btn btn-primary btn-active" text="Login" @tap="loginTap"/>
+        <Button class="btn btn-primary btn-active" text="Register" @tap="registerTap"/>
+    </FlexboxLayout>
+</template>
+
+<script>
+export default {
+    data: () => ({
+        username: null,
+        password: null
+    }),
+    methods: {
+        loginTap() {
+
+        },
+        registerTap() {
+            this.$store.commit('setView', 'Register')
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .input {
+        border-color: seagreen;
+        border-width: 5px;
+        border-radius: 30%;
+        padding: 20px;
+        width: 80%;
+    }
+
+    .layout {
+        background-color: white;
+        animation-name: fade-in;
+        animation-duration: 1s;
+        animation-fill-mode: forwards;
+    }
+
+    @keyframes fade-in {
+        from {
+            opacity: 0;
+        }
+        to {
+            opacity: 1;
+        }
+    }
+</style>
