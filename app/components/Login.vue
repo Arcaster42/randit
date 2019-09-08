3b1caa71ec9aefb634db7591ae1e3509f0dcf3dc
arcaster42 <tj.epperson070@gmail.com>
2019-09-08 16:51:51 +0900

    implemented login

--- app/components/Login.vue
+++ app/components/Login.vue
@@ -1,23 +1,36 @@
 <template>
     <FlexboxLayout class="layout" flexDirection="column" justifyContent="center">
-        <Label alignSelf="center" text="Username"/>
-        <TextField class="input" alignSelf="center" v-model="username" hint="Enter Username"/>
+        <Label alignSelf="center" text="E-Mail"/>
+        <TextField class="input" alignSelf="center" v-model="email" hint="Enter E-mail"/>
         <Label alignSelf="center" text="Password"/>
-        <TextField class="input" alignSelf="center" v-model="password" hint="Enter Username"/>
+        <TextField class="input" alignSelf="center" v-model="password" hint="Enter Password"/>
         <Button class="btn btn-primary btn-active" text="Login" @tap="loginTap"/>
         <Button class="btn btn-primary btn-active" text="Register" @tap="registerTap"/>
     </FlexboxLayout>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
     data: () => ({
-        username: null,
+        email: null,
         password: null
     }),
     methods: {
         loginTap() {
-
+            const userObj = {
+                email: this.email,
+                password: this.password
+            }
+            axios.get('http://192.168.1.6:3000/api/user', { params: userObj })
+            .then((response) => {
+                if (response.data.err) alert(response.data.err)
+                else {
+                    console.log(response.data)
+                }
+            })
+            .catch((err) => alert(err))
         },
         registerTap() {
             this.$store.commit('setView', 'Register')
