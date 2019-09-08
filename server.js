8251415a2fcca97ed82d346872fd2134cc26dfbc
arcaster42 <tj.epperson070@gmail.com>
2019-09-08 15:44:31 +0900

    debugged, implemented duplicate checks

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ const port = process.env.PORT || 3000
 
 //Database
 const mongoose = require('mongoose')
-mongoose.connect('127.0.0.1:27017', { useNewUrlParser: true })
-import { UserModel } from './database/models'
+mongoose.connect('mongodb://localhost:27017/randit', { useNewUrlParser: true })
+const { UserModel } = require('./database/models')
 
 //Authentication
 const bcrypt = require('bcrypt')
@@ -18,20 +18,26 @@ app.get('/api', (req, res) => {
     res.sendStatus(200)
 })
 
-app.post('/api/user', (req, res) => {
+app.post('/api/user', async (req, res) => {
+    console.log('POST USER')
     const userObj = req.body
-    if (!userObj) return { err: 'Missing User Object' }
-    const hash = bcrypt.hashSync(userObj.password, 10)
-    const newUser = new UserModel({
-        first_name: userObj.firstName,
-        last_name: userObj.lastName,
-        email: userObj.email,
-        pass_hash: hash
-    })
-    newUser.save((err, doc) => {
-        if (err) return { err }
-        if (doc) return doc
-    })
+    if (!userObj) res.send({ err: 'Missing User Object' })
+    const search = await UserModel.findOne({ email: userObj.email })
+    if (search) res.send({ err: 'Email Unavailable' })
+    if (!search) {
+        const hash = bcrypt.hashSync(userObj.password, 10)
+        const newUser = new UserModel({
+            first_name: userObj.firstName,
+            last_name: userObj.lastName,
+            username: userObj.username,
+            email: userObj.email,
+            pass_hash: hash
+        })
+        newUser.save((err, doc) => {
+            if (err) res.send({ err })
+            if (doc) res.send(doc)
+        })
+    }
 })
 
 app.listen(port, () => console.log(`Listening on ${port}`))
