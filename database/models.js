ca9142aef0cf3d832ba7359b42aa03e8b6792ac1
arcaster42 <tj.epperson070@gmail.com>
2019-09-08 15:44:40 +0900

    added username

--- database/models.js
+++ database/models.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose')
 
-const UserModel = mongoose.model('User', new Schema({
+const UserSchema = mongoose.Schema({
     first_name: String,
     last_name: String,
+    username: String,
     email: String,
     pass_hash: String
-}))
+})
+
+const UserModel = mongoose.model('User', UserSchema)
 
 module.exports = { UserModel }
