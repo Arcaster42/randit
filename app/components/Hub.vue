e9f25848178bcc8b4d1f627a71838c0c91cbcd7e
arcaster42 <tj.epperson070@gmail.com>
2019-09-11 00:04:24 +0900

    added checkin sub and logic

--- app/components/Hub.vue
+++ app/components/Hub.vue
@@ -1,13 +1,21 @@
 <template>
     <FlexboxLayout class="layout">
-        <Button v-if="!getActive" class="btn btn-primary btn-active" text="New Check-In" @tap="checkinTap"/>
-        <Button v-if="getActive" class="btn btn-primary btn-active" text="Check-In" @tap="checkinTap"/>
+        <Button v-if="!getActive" class="btn btn-primary btn-active" :text="label" @tap="checkinTap"/>
+        <Checkin v-if='showCheckin'/>
+        <Button class="btn btn-danger btn-active" text="Logout" @tap="logoutTap"/>
     </FlexboxLayout>
 </template>
 
 <script>
+import Checkin from './Checkin'
+
 export default {
+    components: {
+        Checkin
+    },
     data: () => ({
+        label: 'New Checkin-In',
+        showCheckin: false,
         buttonText: null
     }),
     mounted: function() {
@@ -24,14 +32,21 @@ export default {
             if (this.$store.state.user.checkin.active) return 'Check-In'
         },
         checkinTap() {
-            if (!this.$store.state.user.checkin.active) {
-                const checkin = { active: true }
-                this.$store.commit('setCheckin', checkin)
-            }
+            if (!this.$store.state.user.checkin.active && !this.showCheckin) {
+                this.showCheckin = true
+                this.label = 'Cancel Check-In'
+            } else
+            if (!this.$store.state.user.checkin.active && this.showCheckin) {
+                this.showCheckin = false
+                this.label = 'New Check-In'
+            } else
             if (this.$store.state.user.checkin.active) {
                 const checkin = { active: false }
                 this.$store.commit('setCheckin', checkin)
             }
+        },
+        logoutTap() {
+            this.$store.commit('setView', 'Login')
         }
     }
 }
@@ -45,6 +60,7 @@ export default {
     .layout {
         flex-direction: column;
         justify-content: center;
+        margin-top: 10px;
         background-color: white;
         animation-name: fade-in;
         animation-duration: 1s;
